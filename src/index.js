f2499bb17bc50f275cdadd50676b7db3c749c52c
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-06-04 23:26:33 -0700

    Add pop up content to menu button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
-console.log('Running...');
+const menuButton = document.querySelector('.menu-button');
+const menuItemContainerElem = document.querySelector('.menu-item-container');
+
+menuItemContainerElem.style.display = 'none';
+
+menuButton.addEventListener('click', () => {
+  if (menuItemContainerElem.style.display === 'none')
+  {
+    menuItemContainerElem.style.display = 'block';
+  }
+  else if (menuItemContainerElem.style.display === 'block')
+  {
+    menuItemContainerElem.style.display = 'none';
+  }
+})
